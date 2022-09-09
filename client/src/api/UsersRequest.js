5286ac319c6a8d5329591f8eaa570b962f7767cd
Doston <71603612+salomcha@users.noreply.github.com>
2022-09-09 12:34:34 +0500

    deno blog

--- client/src/api/UsersRequest.js
+++ client/src/api/UsersRequest.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const URL = 'http://ec2-18-132-64-241.eu-west-2.compute.amazonaws.com:5000/';
+const URL = 'https://sub.doston.me/';
 const API = axios.create({ baseURL: URL });
 
 const config = {
